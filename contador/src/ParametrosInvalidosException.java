3c4b0fa21fa97ffe4951b50498b6b158ab2d37d3
luizfelipe <santiago10felipe@gmail.com>
2024-06-10 14:00:45 -0300

    first commit

---
+++ contador/src/ParametrosInvalidosException.java
@@ -0,0 +1,11 @@
+
+public class ParametrosInvalidosException {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public char[] getMessage() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getMessage'");
+    }
+}
