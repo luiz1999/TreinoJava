aaf8afd7fb488cc4c24c6c7a227e789c56a236a4
luizfelipe <santiago10felipe@gmail.com>
2024-06-13 01:53:00 -0300

    completo

--- Desafios/src/ControleSimplesDeSaques.java
+++ Desafios/src/ControleSimplesDeSaques.java
@@ -4,11 +4,13 @@ public class ControleSimplesDeSaques {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o limite diário de saque.");
         double limiteDiario = scanner.nextDouble();
 
         //Loop for para iterar sobre os saques
         for(int i = 1; i < limiteDiario ; i++) {
 
+            System.out.println("Informe o valor do saque.");
             double valorSaque = scanner.nextDouble();
 
             if (valorSaque == 0) {
@@ -19,6 +21,8 @@ public class ControleSimplesDeSaques {
                 break; //Encerra o loop
             } else {
                 //TODO: Atualizar o limite diário e imprimir a saída no formato dos exemplos.
+                double novoSaldo = limiteDiario - valorSaque;
+                System.out.printf("Saque realizado com sucesso. Limite restante: %.1f%n", novoSaldo);
             }
         }
 
